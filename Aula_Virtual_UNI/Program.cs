d00dfdf1ffeb235a3c94f4b9d062fa4dd4be0c32
Gerald1692 <geraldariasgarcia06@gmail.com>
2025-11-09 22:24:45 -0600

    Creacion de la Vista de Estudiante
    Creacion de la Vista de Estudiante

--- Aula_Virtual_UNI/Program.cs
+++ Aula_Virtual_UNI/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Login}/{id?}");
+    pattern: "{controller=Estudiante}/{action=Estudiante}/{id?}");
 
 app.Run();
