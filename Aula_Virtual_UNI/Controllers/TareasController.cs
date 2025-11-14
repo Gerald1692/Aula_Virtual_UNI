25761baeb20065e119d8aafa16039deea6ea0a30
Legolas <157653932+Legolasfk@users.noreply.github.com>
2025-11-13 20:16:02 -0600

    actalización de vistas

---
+++ Aula_Virtual_UNI/Controllers/TareasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula_Virtual_UNI.Controllers
+{
+    public class TareasController : Controller
+    {
+        public IActionResult V_Tareas()
+        {
+            return View();
+        }
+    }
+}
