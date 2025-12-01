54e88384fc037b8f2adaf47c4f413187b75f50b0
Legolas <157653932+Legolasfk@users.noreply.github.com>
2025-11-30 18:39:02 -0600

    feat
    mejoras

--- Aula_Virtual_UNI/Controllers/CrearCuentaController.cs
+++ Aula_Virtual_UNI/Controllers/CrearCuentaController.cs
@@ -53,15 +53,15 @@ namespace Aula_Virtual_UNI.Controllers
             {
                 var u = respuesta.ValorRetorno;
 
-                HttpContext.Session.SetString("Nombre", u.NombreUsuario ?? "");
-                HttpContext.Session.SetString("Rol", u.Rol ?? "");
+                HttpContext.Session.SetString("Nombre", u.NombreCompleto ?? "");
+                HttpContext.Session.SetString("Rol", u.RolId.ToString() ?? "");
 
                 var ConfigCookie = new CookieOptions()
                 {
                     Expires = DateTime.Now.AddMinutes(30),
                 };
 
-                Response.Cookies.Append("IdUsuario", Convert.ToString(u.IdUsuario), ConfigCookie);
+                Response.Cookies.Append("IdUsuario", Convert.ToString(u.Id), ConfigCookie);
             }
 
             return respuesta;
